44cc16d9f0174e8e6467aa24654240937d35a468
Yashar aliabbasi <Yasharvc@yahoo.com>
2018-10-31 15:33:34 +0330

    Making Dynamic

--- Shared/JS/ElementBlocker.js
+++ Shared/JS/ElementBlocker.js
@@ -1,41 +1,94 @@
+var BlockWithoutOpacity = 1;
+var BlockWithOpacity = 2;
+
 function Blocker(blockedElement) {
     blockedElement = blockedElement.replace('#', '');
-    var WithoutOpacity = 1;
-    var WithOpacity = 2;
-
+    var theBlocker = null;
     this.blockedElement = getElement(blockedElement);
 
     this.showLoader = function (type) {
         var cssString = prepareStyle(type);
 
         this.theBlocker = document.createElement('div');
-        this.theBlocker.className = "w3-center";
+        this.theBlocker.className = "w3-center FadeIn";
 
-        this.theBlocker.innerHTML = "<i style='display: table-cell;vertical-align: middle;opcaity:1;' class='w3-text-green fa fa-spin fa-refresh fa-5x'></i>";
+        this.theBlocker.innerHTML = "<i style='display: table-cell;vertical-align: middle;opcaity:1;' class='w3-text-green fa fa-spin fa-refresh fa-5x'></i>" +
+            '<div class="w3-light-grey" style="width:100%;"><div id="__progressbar" class="w3-container w3-green w3-center w3-tiny" style="width:0%;position:fixed;left:0px;margin:10px;">0%</div></div>';
         this.theBlocker.style.cssText = cssString;
 
         this.blockedElement.appendChild(this.theBlocker);
+        theBlocker = this.theBlocker;
     };
 
     this.hideLoader = function () {
+        this.theBlocker.className = this.theBlocker.className.replace(/FadeIn/g, '');
+        this.theBlocker.className += 'FadeOut';
         this.blockedElement.removeChild(this.theBlocker);
+        theBlocker = null;
     };
 
-    var prepareStyle = function(type){
-        type = isNull(type) ? WithoutOpacity : type;
+    var prepareStyle = function (type) {
+        type = isNull(type) ? BlockWithoutOpacity : type;
         var res = "";
-        if(type === WithOpacity)
+        if (type === BlockWithOpacity)
             res += "background-color:rgba(0,0,0,0.8);";
-        else if(type === WithoutOpacity)
+        else if (type === BlockWithoutOpacity)
             res += "background-color:black;";
-        res += "border:none;margin:0;padding:0;position:absolute;width:100%;display: table;height:100%;top:0;left:0;min-height:100%;max-height:100%;";
+        res += "border:none;margin:0;padding:0;position:absolute;width:100%;display: table;height:100vh;top:0;left:0;";
         return res;
     };
+
+    window.addEventListener('resize', function () {
+        if (theBlocker != null) {
+            theBlocker.style.height = window.innerHeight;
+            this.console.log('sdasd');
+        }
+    });
 }
 
-function getElement(expToSearch){
+
+
+var bodyBlocker;
+
+function getElement(expToSearch) {
     return document.getElementById(expToSearch) ||
-    document.getElementsByClassName(expToSearch)[0] ||
-    document.getElementsByTagName(expToSearch)[0] ||
-    document.getElementsByName(expToSearch)[0];
+        document.getElementsByClassName(expToSearch)[0] ||
+        document.getElementsByTagName(expToSearch)[0] ||
+        document.getElementsByName(expToSearch)[0];
+}
+
+function BlockEntirePage() {
+    if (isNull(bodyBlocker))
+        bodyBlocker = new Blocker('body');
+    bodyBlocker.showLoader();
+}
+
+function UnblockEntirePage() {
+    if (isNull(bodyBlocker))
+        bodyBlocker = new Blocker('body');
+    bodyBlocker.hideLoader();
+}
+
+function changeProgress(percent) {
+    percent = parseInt(percent);
+    if (percent >= 0 && percent <= 100) {
+        var elem = document.getElementById("__progressbar");
+        var width = percent;
+        elem.style.width = width + '%';
+        elem.innerHTML = width * 1 + '%';
+    }
+}
+
+function move() {
+    var id = setInterval(frame, 10);
+    var width = 0;
+
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            changeProgress(width);
+        }
+    }
 }
--- Shared/JS/Utility.js
+++ Shared/JS/Utility.js
@@ -1,3 +1,64 @@
-function isNull(obj){
+var scripts = [];
+var csses = [];
+function isNull(obj) {
     return obj === null || typeof obj === 'undefined';
+}
+
+function getBottomScript() {
+    return document.getElementById('__bottomscript__');
+}
+
+function getTopScript() {
+    return document.getElementById('__topscript__');
+}
+
+function getBottomCSS() {
+    return document.getElementById('__bottomcss__');
+}
+
+function getTopCSS() {
+    return document.getElementById('__topcss__');
+}
+function getBody() {
+    return document.getElementById('__body__');
+}
+function addJSToBottom(JSCode) {
+    var fileref = document.createElement('script');
+    fileref.setAttribute("type", "text/javascript");
+    fileref.innerHTML = JSCode;
+    if (!isNull(fileref)){
+        getBottomScript().appendChild(fileref);
+        scripts.push(fileref);
+    }
+}
+
+function addCSSToTop(CSSCode) {
+    var fileref = document.createElement("style");
+    fileref.innerHTML = CSSCode;
+    if (!isNull(fileref)){
+        getTopCSS().appendChild(fileref);
+        csses.push(fileref);
+    }
+}
+
+function setBody(HTMLCode){
+    getBody().innerHTML = HTMLCode;
+}
+
+function appendBody(HTMLCode){
+    getBody().innerHTML += HTMLCode;
+}
+function clearBody(){
+    setBody('');
+}
+function clearJS(){
+    for(var i = 0 ; i < scripts.length ; i++){
+        getBottomScript().removeChild(scripts[i]);
+    }
+    scripts = [];
+}
+function getFunctionList(exp){
+    var regExp = /function\s*[\w]*\(([^)]*)\)/;
+    var matches = regExp.exec("I expect five hundred dollars ($500).");
+    console.log(matches[1]);
 }
